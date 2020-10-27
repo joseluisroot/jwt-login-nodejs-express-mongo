0e357d8c01e92beed459e59d9b4625d2afd4e40f
Pablo Sabater <pabloosabaterr@gmail.com>
2020-10-28 00:04:06 +0100

    refactor(routes): cleanup auth routes

---
+++ src/routes/auth.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+const AuthCtrl = require('../controllers/auth.controllers')
+const isEmailExist = require('../middlewares/register')
+
+const { Router } = require('express');
+
+const router = Router()
+
+
+
+router.post('/login', AuthCtrl.signIn)
+
+router.post('/register', isEmailExist, AuthCtrl.register );
+
+module.exports = router;
