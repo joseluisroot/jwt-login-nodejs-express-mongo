5df0ba1ed8cbd7b7cf5805016af3a8a9aec4f78d
José Luis Reyes <joseluis.r85@gmail.com>
2020-10-29 09:12:49 -0600

    agregando ruta para recuperar la data del usuario

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,11 +3,14 @@ const AuthCtrl = require('../controllers/auth.controllers')
 const isEmailExist = require('../middlewares/register')
 
 const { Router } = require('express');
+const verifyToken = require('../middlewares/validate-token');
 
 const router = Router();
 
 router.post('/login', AuthCtrl.signIn);
 
 router.post('/register', isEmailExist, AuthCtrl.register);
+
+router.get('/data', verifyToken, AuthCtrl.getDataUserbyToken);
 
 module.exports = router;
