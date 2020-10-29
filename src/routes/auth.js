9709765a7fcf60d565096d7eadf883df2e29f04b
José Luis Reyes <joseluis.r85@gmail.com>
2020-10-28 23:34:39 -0600

    clean auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,12 +4,10 @@ const isEmailExist = require('../middlewares/register')
 
 const { Router } = require('express');
 
-const router = Router()
+const router = Router();
 
+router.post('/login', AuthCtrl.signIn);
 
-
-router.post('/login', AuthCtrl.signIn)
-
-router.post('/register', isEmailExist, AuthCtrl.register );
+router.post('/register', isEmailExist, AuthCtrl.register);
 
 module.exports = router;
