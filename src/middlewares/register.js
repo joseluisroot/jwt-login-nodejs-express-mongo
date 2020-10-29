952bc2ff630dc33a68e899e08e1f25ae379d22f7
José Luis Reyes <joseluis.r85@gmail.com>
2020-10-28 23:31:55 -0600

    reparando middlewares register

--- src/middlewares/register.js
+++ src/middlewares/register.js
@@ -1,8 +1,12 @@
+const User = require("../models/User");
+
 const isEmailExist = async (req, res, next) => {
-        await User.findOne({ email: req.body.email });
-    if (isEmailExist) {
-    return res.status(400).json({ error: "Email ya registrado" });
+        
+    const emailVerification  = await User.findOne({ email: req.body.email });
+    if (emailVerification) {
+        return res.status(400).json({ error: "Email ya registrado" });
     }
+    next();
 }
 
 module.exports = isEmailExist
